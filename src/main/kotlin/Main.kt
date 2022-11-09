8a13fb57b9faf0e332fa4b5d156a1f7e60abb17f
Pagão <gabrielpaganinibarroso@hotmail.com>
2022-11-09 19:09:15 -0300

    usando named arguments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     val calculadora = Calculadora()
     println(calculadora.calcular(10,10))
-    println(calculadora.calcular(80,10,"-"))
-    println(calculadora.calcular(100,10,"*"))
+    println(calculadora.calcular(x = 80, y = 10, operador = "-"))
+    println(calculadora.calcular(x = 100,y = 10, operador = "*"))
     println(calculadora.calcular(1190,10,"/"))
 }
